0523183d0f74a2512743e653e04078da79d1728c
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-11 12:19:37 -0300

    add condicional if

--- adivina_tu_numero.py
+++ adivina_tu_numero.py
@@ -12,4 +12,12 @@ def adivina_el_numero(x):
     predicción = 0 
 
     while predicción != numero_aleatoreo:
-        predicción = input(f"Adivina el número entre 1 y {x}: ")
+        # el usuario ingresa un numero
+        predicción = int(input(f"Adivina el número entre 1 y {x}: ")) # f-string
+
+        if predicción < numero_aleatoreo:
+            print("Intenta otra vez. Este número es muy pequeño.")
+        elif predicción > numero_aleatoreo:
+            print("Intenta otra vez. Este número es muy grande.")
+
+    print(f"¡Felicitaciones! adivinaste el número {numero_aleatoreo} correctamente.")
