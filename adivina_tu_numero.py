b36dd320230701b7b651b14dd9b310035e6d9688
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-11 11:52:21 -0300

    add random

--- adivina_tu_numero.py
+++ adivina_tu_numero.py
@@ -1,5 +1,10 @@
+import random
+
 def adivina_el_numero(x):
 
     print("************************")
     print(" Bienvenido(a) al juego ")
     print("************************")
+    print(" tu meta es adivinar el número generado por la computadora.")
+
+    numero_aleatoreo = random
